b59efc7ad1c42099dd62274ec5df77e2ce6504a9
tada20001 <tada20001@gmail.com>
2021-04-09 19:08:23 +0900

    Setting static files

--- tada/urls.py
+++ tada/urls.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='root.html'), name='root'),
 ]
 
 if settings.DEBUG:
