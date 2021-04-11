d7d0c09bb94c109b1ae935486226c1a61836a191
tada20001 <tada20001@gmail.com>
2021-04-11 19:39:05 +0900

    EP13. Creating user page

--- instagram/views.py
+++ instagram/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
 from django.contrib import messages
 from .models import Tag, Post
 from .forms import PostForm
@@ -24,3 +25,9 @@ def post_new(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, "instagram/post_detail.html", {'post': post, })
+
+
+def user_page(request, username):
+    page_user = get_object_or_404(get_user_model(), username=username, is_active=True)
+    post_list = Post.objects.filter(author=page_user)
+    return render(request, 'instagram/user_page.html', {'page_user': page_user, 'post_list': post_list,})
