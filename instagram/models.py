e3aa21039c5f71c778b35886fc272391ddd0ea7e
tada20001 <tada20001@gmail.com>
2021-04-13 14:07:39 +0900

    EP20. Creating Like/Dislike icon

--- instagram/models.py
+++ instagram/models.py
@@ -4,11 +4,12 @@ from django.db import models
 import re
 
 class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="my_post_set", on_delete=models.CASCADE)
     photo = models.ImageField(upload_to="instagram/post/%Y/%m/%d")
     caption = models.CharField(max_length=500)
     tag_set = models.ManyToManyField('Tag', blank=True)
     location = models.CharField(max_length=100, blank=True)
+    like_user_set = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name="like_post_set")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
@@ -26,9 +27,17 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse("instagram:post_detail", kwargs={"pk": self.pk})
 
+    def is_like_user(self, user):
+        return self.like_user_set.filter(pk=user.pk).exists()
+
+    class Meta:
+        ordering = ['-id']
+
 
 class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
         return self.name
+
+
--- instagram/views.py
+++ instagram/views.py
@@ -17,7 +17,7 @@ def index(request):
             Q(author__in=request.user.following_set.all())
         )\
         .filter(
-            created_at__lte=timesince) # 3일이하 경과된 포스팅만 가져오겠다.
+            created_at__gte=timesince) # 3일이하 경과된 포스팅만 가져오겠다.
 
     suggested_user_list = get_user_model().objects.all().exclude(pk=request.user.pk)\
                             .exclude(pk__in=request.user.following_set.all())[:3]
@@ -50,6 +50,24 @@ def post_detail(request, pk):
     return render(request, "instagram/post_detail.html", {'post': post, })
 
 
+@login_required
+def post_like(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.like_user_set.add(request.user)
+    messages.success(request, f"포스팅#{post.pk}를 좋아합니다.")
+    redirect_url = request.META.get("HTTP_REFERER", "root")
+    return redirect(redirect_url)
+
+
+@login_required
+def post_dislike(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.like_user_set.remove(request.user)
+    messages.success(request, f"포스팅#{post.pk}좋아요를 취소합니다.")
+    redirect_url = request.META.get("HTTP_REFERER", "root")
+    return redirect(redirect_url)
+
+
 def user_page(request, username):
     page_user = get_object_or_404(get_user_model(), username=username, is_active=True)
 
@@ -66,3 +84,5 @@ def user_page(request, username):
         'post_list_count': post_list_count,
         'is_follow': is_follow,
         })
+
+
